2a79811f360f782c4714d00342c99153e591dce0
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-10-23 17:52:28 +0900

    Testing of page navigation

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,8 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Home from "../pages/index";
+import "setimmediate";
 
 it("Should render hello text", () => {
   render(<Home />);
-  expect(screen.getByText("Hello Nextjs")).toBeInTheDocument();
+  expect(screen.getByText("Welcome to Nextjs")).toBeInTheDocument();
 });
---
+++ pages/context-page.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+const ContextPage: React.FC = () => {
+  return (
+    <Layout title="Context">
+      <p className="text-4xl">Context page</p>
+    </Layout>
+  );
+};
+export default ContextPage;
